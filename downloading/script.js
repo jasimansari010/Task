236f2dff92e403893cfd4d17cdd506213041b009
Jasim-10 <mdjasimansari010@gmail.com>
2026-07-11 22:45:00 +0530

    Downloading Effect

---
+++ downloading/script.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector('button');
+let h2 = document.querySelector('h2');
+let inner = document.querySelector('.inner');
+let p = document.querySelector('p')
+
+let grow = 0;
+btn.addEventListener('click',()=>{
+    btn.style.pointerEvents = 'none';
+    let num = Math.floor(Math.random()*50)+30;
+    let int = setInterval(()=>{
+        grow++;
+        h2.innerHTML = grow +'%';
+        inner.style.width = grow +'%';
+        p.innerHTML = 'Downloading.... &#128640;'
+    },num);
+
+    setTimeout(()=>{
+        clearInterval(int)
+        btn.innerHTML = 'Downloaded';
+        btn.style.opacity = 0.5;
+        p.innerHTML = `Downloaded in ${num/10} seconds `;
+    },num*100);
+})
